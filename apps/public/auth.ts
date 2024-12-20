83e61e608a5f1213cc5ca03e875752c264f6e357
abdurrahman rajab <abdurrahman.rajab@ayasofyazilim.com>
2024-12-20 10:57:21 +0300

    chore: add token type and token checks

--- apps/public/auth.ts
+++ apps/public/auth.ts
@@ -1,8 +1,22 @@
-import NextAuth, { User } from "next-auth"
+import NextAuth, { AuthError, User } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
-const TOKEN_URL = `${process.env.BASE_URL}/connect/token`;
-const OPENID_URL = `${process.env.BASE_URL}/.well-known/openid-configuration`;
+const TOKEN_URL = `${process.env.AUTH_URL}/connect/token`;
+const OPENID_URL = `${process.env.AUTH_URL}/.well-known/openid-configuration`;
+
+type Token = {
+  access_token: string
+  token_type: string
+  expires_in: number
+  id_token: string
+  refresh_token: string
+}
+
+// create a function to check object is of type Token 
+function isToken(obj: unknown): obj is Token {
+  if (typeof obj !== 'object' || obj === null) return false;
+  return 'access_token' in obj;
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -43,23 +57,55 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           headers: myHeaders,
           body: urlencoded,
         };
+
         const response = await fetch(TOKEN_URL, requestOptions);
-        console.log("response", response);
-        const json: User = await response.json();
+        const json = await response.json();
         user = json;
-        console.log("json user", user)
-        if ("error" in user && "error_description" in user) {
-          throw new Error(user.error + ": " + user.error_description);
+        // if (isToken(json)) {
+        //   let user: User = { email: "" + credentials.email }
+        //   // const userResponse = await fetch(`${process.env.BASE_URL}/api/user`, {
+        //   //   headers: {
+        //   //     Authorization: `Bearer ${json.access_token}`,
+        //   //   },
+        //   // });
+        //   // user = await userResponse.json();
+        //   // return { user: json, token: json } as User;
+        //   return json;
+        // }
+        if ("error" in json && "error_description" in json) {
+          // return { error: json.error + ": " + json.error_description }; 
+          throw new AuthError(json.error + ": " + json.error_description);
         }
+        console.log("user", user)
+
         if (!user) {
           // No user found, so this is their first attempt to login
           // Optionally, this is also the place you could do a user registration
           throw new Error("Invalid credentials.")
         }
+        // id?: string
+        // name?: string | null
+        // email?: string | null
+        // image?: string | null
+
 
         // return user object with their profile data
         return user
       },
     }),
   ],
+  callbacks: {
+    session({ session, token, user }) {
+      // `session.user.address` is now a valid property, and will be type-checked
+      // in places like `useSession().data.user` or `auth().user`
+      console.log("session", session, token, user)
+      return {
+        ...session,
+        user: {
+          ...session.user,
+        },
+      }
+    },
+  },
+
 })
