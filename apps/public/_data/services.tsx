97990d560b2a45365c73e81d525e14f72cb8c475
Ertuğrulcan Bakas <ecbakas@gmail.com>
2024-12-25 14:50:06 +0300

    feat: refactor ServicesSection to use external services data and enhance card styling

---
+++ apps/public/_data/services.tsx
@@ -0,0 +1,16 @@
+import { HandCoins, Heart } from "lucide-react";
+
+export const services = [
+  {
+    icon: HandCoins,
+    title: "Give Donation",
+    description:
+      "The evidence is clear. The ability to learn to read is single significant factor potential. Became a volunteer people and the community.",
+  },
+  {
+    icon: Heart,
+    title: "Become Volunteer",
+    description:
+      "The evidence is clear. The ability to learn to read is single significant factor potential. Became a volunteer people and the community.",
+  },
+];
