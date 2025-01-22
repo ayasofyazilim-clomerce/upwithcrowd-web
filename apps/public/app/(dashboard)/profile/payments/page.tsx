408050a9330aee97d79973fae6a1ebd97c7bc316
muhammed-gumus <Mami040953>
2025-01-22 15:30:55 +0300

    feat(payments): implement payment transaction retrieval and enhance payments page

--- apps/public/app/(dashboard)/profile/payments/page.tsx
+++ apps/public/app/(dashboard)/profile/payments/page.tsx
@@ -1,5 +1,37 @@
 import PaymentsPage from "./client";
+import { getApiPaymentTransaction } from "@/actions/upwithcrowd/payment/action";
+import { getProjectApi } from "@/actions/upwithcrowd/project/action";
 
-export default function Page() {
-  return <PaymentsPage />;
+export default async function Page() {
+  const paymentsResponse = await getApiPaymentTransaction({
+    maxResultCount: 100,
+  });
+
+  if (paymentsResponse.type !== "success")
+    return <>{paymentsResponse.message}</>;
+
+  const projectsResponse = await getProjectApi();
+
+  if (projectsResponse.type !== "success") {
+    return <>{projectsResponse.message}</>;
+  }
+
+  const projects = projectsResponse.data.items || [];
+
+  const payments =
+    paymentsResponse.data.items?.map((payment) => {
+      const project = projects.find(
+        (project) => project.id === payment.projectID,
+      );
+
+      return {
+        projectName: project?.projectName || "Unknown Project",
+        transactionId: payment.relatedTransactionID,
+        amount: payment.amount,
+        paymentStatus: payment.paymentStatus,
+        paymentType: payment.paymentType,
+      };
+    }) ?? [];
+
+  return <PaymentsPage payments={payments} />;
 }
