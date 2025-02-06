d4814e936aee23703463bbe51117d9967a4eefb4
muhammed-gumus <Mami040953>
2025-02-06 10:00:46 +0300

    feat(projects): add API for fetching project details and update ProjectDetails component

--- apps/public/app/(landing)/projects/[id]/page.tsx
+++ apps/public/app/(landing)/projects/[id]/page.tsx
@@ -1,19 +1,15 @@
 import ProjectDetails from "./client";
-import {getPublicProjectDetailsApi, getPublicProjectDetailsFundingApi} from "@/actions/upwithcrowd/project/action";
-
+import {getPublicProjectDetailByIdApi} from "@/actions/upwithcrowd/public-project/actions";
 export default async function Page({params}: {params: {id: string}}) {
   const {id} = params;
 
-  const projectDetailsResponseBasics = await getPublicProjectDetailsApi(id);
-  const projectDetailsResponseFunding = await getPublicProjectDetailsFundingApi(id);
+  const projectDetailsResponseBasics = await getPublicProjectDetailByIdApi(id);
 
   if (projectDetailsResponseBasics.type !== "success") return <>{projectDetailsResponseBasics.message}</>;
 
-  if (projectDetailsResponseFunding.type !== "success") return <>{projectDetailsResponseBasics.message}</>;
-
   return (
     <div className="bg-background min-h-screen">
-      <ProjectDetails funding={projectDetailsResponseFunding.data} basics={projectDetailsResponseBasics.data} />
+      <ProjectDetails data={projectDetailsResponseBasics.data} />
     </div>
   );
 }
