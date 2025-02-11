a45cced08c3cc5efa2ba467099bb5c0df0cc1a17
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-02-11 08:05:57 +0300

    feat(eslint): migrate from .eslintrc.json to .eslintrc.js and update rules

---
+++ apps/public/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["unused-imports"],
+  extends: ["@repo/eslint-config/next.js", "eslint:recommended"],
+  ignorePatterns: ["**/language-management/form.tsx", "**/vendor/*.js"],
+  rules: {
+    /* Should stay disabled */
+    camelcase: "off",
+    "import/no-cycle": "off",
+    "no-return-await": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
