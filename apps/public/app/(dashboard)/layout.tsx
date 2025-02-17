624a75ac6b4d67fa21c1edc6e8cd724ae811e2f4
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-02-17 09:39:04 +0300

    chore:layout

--- apps/public/app/(dashboard)/layout.tsx
+++ apps/public/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <section className="min-h-screen">
       <Header />
-      <div className="bg-muted mb-10 size-full overflow-auto pb-10">{children}</div>
+      <div className="bg-muted size-full h-[calc(100vh-6rem)] overflow-auto">{children}</div>
     </section>
   );
 }
