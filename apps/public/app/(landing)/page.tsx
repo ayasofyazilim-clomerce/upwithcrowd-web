26839e812ca6680e144ec8e36ddac4e12fc74c05
muhammed-gumus <Mami040953>
2025-02-21 14:28:43 +0300

    feat: enhance ProjectSlider to fetch and display public projects with improved sorting and details

--- apps/public/app/(landing)/page.tsx
+++ apps/public/app/(landing)/page.tsx
@@ -8,13 +8,17 @@ import MissionsSection from "@/components/mission-section";
 import OrganizationNumbers from "@/components/organization-numbers";
 import ServicesSection from "@/components/services-section";
 import Testimonials from "@/components/testimoni";
+import {getPublicProjectsApi} from "@/actions/upwithcrowd/public-project/actions";
 
-export default function Page() {
+export default async function Page() {
+  const publicProjectsResponse = await getPublicProjectsApi({});
   return (
     <div className="bg-background min-h-screen">
       <main>
         <HeroSection />
-        <ProjectSlider />
+        <ProjectSlider
+          projects={typeof publicProjectsResponse.data === "string" ? [] : publicProjectsResponse.data.items || []}
+        />
         <DetailsSection />
         <ServicesSection />
         <MissionsSection />
