5853415674c9a5b9385a0a42d37c910c5afdf9fd
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-02-28 15:34:43 +0300

    style: format code for consistency in Providers component

--- apps/public/app/providers/providers.tsx
+++ apps/public/app/providers/providers.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import type { Session } from "@repo/utils/auth";
-import { NovuProvider } from "@repo/ui/providers/novu";
-import { SessionProvider } from "@repo/utils/auth";
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import type {Session} from "@repo/utils/auth";
+import {NovuProvider} from "@repo/ui/providers/novu";
+import {SessionProvider} from "@repo/utils/auth";
+import {Toaster} from "@/components/ui/sonner";
+import {TooltipProvider} from "@/components/ui/tooltip";
 import dynamic from "next/dynamic";
-import { Skeleton } from "@/components/ui/skeleton";
-import type { Member } from "./member";
+import {Skeleton} from "@/components/ui/skeleton";
+import type {Member} from "./member";
 
 const MemberProvider = dynamic(() => import("./member"), {
   ssr: false,
@@ -20,13 +20,16 @@ interface ProvidersProps {
   members: Member[];
 }
 
-export default function Providers({ children, session, currentMember, members }: ProvidersProps) {
+export default function Providers({children, session, currentMember, members}: ProvidersProps) {
   return (
     <SessionProvider session={session}>
       <TooltipProvider>
         <Toaster richColors />
         <MemberProvider currentMember={currentMember} members={members}>
-          <NovuProvider appId={process.env.NOVU_APP_IDENTIFIER || ""} appUrl={process.env.NOVU_APP_URL || ""} subscriberId={session?.user?.sub || "67b8674f58411ad400a054e9"}>
+          <NovuProvider
+            appId={process.env.NOVU_APP_IDENTIFIER || ""}
+            appUrl={process.env.NOVU_APP_URL || ""}
+            subscriberId={session?.user?.sub || "67b8674f58411ad400a054e9"}>
             {children}
           </NovuProvider>
         </MemberProvider>
