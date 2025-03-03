5d3c02b7f927e9f37f3d620085ea4ad9486957d0
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-03-03 09:56:47 +0300

    feat: integrate notification system into layout and header components

--- apps/public/app/[lang]/(dashboard)/layout.tsx
+++ apps/public/app/[lang]/(dashboard)/layout.tsx
@@ -1,9 +1,20 @@
+import {auth} from "@repo/utils/auth/next-auth";
 import Header from "@/components/header";
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default async function Layout({children}: {children: React.ReactNode}) {
+  const session = await auth();
+  let notification;
+  if (session && process.env.NOVU_APP_IDENTIFIER && process.env.NOVU_APP_URL) {
+    notification = {
+      appId: process.env.NOVU_APP_IDENTIFIER,
+      appUrl: process.env.NOVU_APP_URL,
+      subscriberId: session.user?.sub || "",
+      langugageData: {},
+    };
+  }
   return (
     <section className="h-full">
-      <Header />
+      <Header appName={process.env.APPLICATION_NAME || "UPWITHCROWD"} notification={notification} />
       {children}
     </section>
   );
--- apps/public/app/[lang]/(landing)/layout.tsx
+++ apps/public/app/[lang]/(landing)/layout.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="h-screen overflow-auto">
-      <Header />
+      <Header appName={process.env.APPLICATION_NAME || "UPWITHCROWD"} />
       <main className="flex-grow">{children}</main>
       <Footer />
     </section>
