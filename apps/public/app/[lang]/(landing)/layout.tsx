9d36e404260b1c075faa03365c2bef948f8114c3
muhammed-gumus <Mami040953>
2025-03-05 10:30:18 +0300

    fix: update layout section to prevent overflow and improve display

--- apps/public/app/[lang]/(landing)/layout.tsx
+++ apps/public/app/[lang]/(landing)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <section className="h-screen overflow-auto">
+    <section className="overflow-hidden">
       <Header appName={process.env.APPLICATION_NAME || "UPWITHCROWD"} />
       <main className="flex-grow">{children}</main>
       <Footer />
