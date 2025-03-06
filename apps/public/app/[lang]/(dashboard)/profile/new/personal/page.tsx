6625e7617e7091fdc776e457339b5b94c7ed5b04
Eren <ahmeterenodaci@gmail.com>
2025-03-06 11:00:02 +0300

    feat(public): refactor form submission in NewBusinessAccount and NewPersonalAccount components to use promise-based handling & redirection from serverside for quick response

--- apps/public/app/[lang]/(dashboard)/profile/new/personal/page.tsx
+++ apps/public/app/[lang]/(dashboard)/profile/new/personal/page.tsx
@@ -1,9 +1,38 @@
+"use server";
+
+import ErrorComponent from "@repo/ui/components/error-component";
+import {structuredError} from "@repo/utils/api";
+import {isRedirectError, permanentRedirect} from "next/dist/client/components/redirect";
+import {getResourceData} from "@/language/core/Default";
+import {getMemberApi} from "@/actions/upwithcrowd/member/actions";
 import NewPersonalAccount from "./client";
 
-export default function NewBusinessAccountServer() {
-  return (
-    <div>
-      <NewPersonalAccount />
-    </div>
-  );
+async function getApiRequests() {
+  try {
+    const requiredRequests = await Promise.all([]);
+
+    const optionalRequests = await Promise.allSettled([getMemberApi()]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
+
+export default async function Page({params}: {params: {lang: string}}) {
+  const {lang} = params;
+  const {languageData} = await getResourceData(lang);
+  const apiRequests = await getApiRequests();
+  if ("message" in apiRequests) {
+    return <ErrorComponent languageData={languageData} message={apiRequests.message} />;
+  }
+  const [memberResponse] = apiRequests.optionalRequests;
+
+  if (memberResponse.status === "fulfilled" && memberResponse.value.data.items?.length !== 0) {
+    return permanentRedirect(`/${lang}/profile`);
+  }
+
+  return <NewPersonalAccount />;
 }
