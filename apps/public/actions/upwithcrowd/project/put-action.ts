97c1353d3f438960adbf831cd801e44b89f178c7
Eren <ahmeterenodaci@gmail.com>
2025-03-06 11:49:06 +0300

    feat(public/project): add API function to update project status and integrate with finish project flow

--- apps/public/actions/upwithcrowd/project/put-action.ts
+++ apps/public/actions/upwithcrowd/project/put-action.ts
@@ -1,7 +1,11 @@
 "use server";
-import {getUpwithcrowd} from "@/utils/client";
-import {PutApiProjectByIdData, PutApiProjectByIdFundingData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import type {
+  PutApiProjectByIdData,
+  PutApiProjectByIdFundingData,
+  type PutApiProjectByIdStatusData,
+} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
 import {structuredError, structuredResponse} from "@repo/utils/api";
+import {getUpwithcrowd} from "@/utils/client";
 
 export async function putProjectBasicsByIdApi(data: PutApiProjectByIdData) {
   try {
@@ -17,6 +21,15 @@ export async function putProjectFundingByIdApi(data: PutApiProjectByIdFundingDat
   try {
     const client = await getUpwithcrowd();
     const dataResponse = await client.project.putApiProjectByIdFunding(data);
+    return structuredResponse(dataResponse);
+  } catch (error) {
+    return structuredError(error);
+  }
+}
+export async function putProjectStatusByIdApi(data: PutApiProjectByIdStatusData) {
+  try {
+    const client = await getUpwithcrowd();
+    const dataResponse = await client.project.putApiProjectByIdStatus(data);
     return structuredResponse(dataResponse);
   } catch (error) {
     return structuredError(error);
