7e81dfa76b17ca8cd5d1dfdb33bf00444e5084fe
Eren <ahmeterenodaci@gmail.com>
2025-03-10 10:14:46 +0300

    feat: implement MIME types management page and update API actions

--- packages/actions/upwithcrowd/mime-type/actions.ts
+++ packages/actions/upwithcrowd/mime-type/actions.ts
@@ -1,16 +1,16 @@
 "use server";
 
 import type {GetApiMimeTypeData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
-import {structuredError, structuredResponse} from "@repo/utils/api";
+import {structuredError, structuredSuccessResponse} from "@repo/utils/api";
 import type {Session} from "next-auth";
-import {getUpwithcrowdClient} from "upwithcrowd/lib";
+import {getUpwithcrowdClient} from "../lib";
 
-export async function getRolesByIdClaimsApi(data: GetApiMimeTypeData, session: Session | null) {
+export async function getMimeTypesApi(data: GetApiMimeTypeData, session: Session | null) {
   try {
     const client = await getUpwithcrowdClient(session);
     const response = await client.mimeType.getApiMimeType(data);
-    return structuredResponse(response);
+    return structuredSuccessResponse(response);
   } catch (error) {
-    return structuredError(error);
+    throw structuredError(error);
   }
 }
