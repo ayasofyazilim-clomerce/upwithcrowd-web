70471f3ed634776e65c4b7b01af8159aadb4b81c
muhammed-gumus <Mami040953>
2025-03-12 09:45:26 +0300

    feat: implement document management features including upload and listing for organization profiles

---
+++ apps/public/app/[lang]/(dashboard)/profile/documents/document-upload/page.tsx
@@ -0,0 +1,49 @@
+import {getFileTypeGroupTestApi} from "@repo/actions/upwithcrowd/file-type-group/actions";
+import ErrorComponent from "@repo/ui/components/error-component";
+import type {Ruleset} from "@repo/ui/upwithcrowd/file-upload";
+import {FileUpload} from "@repo/ui/upwithcrowd/file-upload";
+import {structuredError} from "@repo/utils/api";
+import {auth} from "@repo/utils/auth/next-auth";
+import {isRedirectError} from "next/dist/client/components/redirect";
+import {getResourceData} from "@/language/core/AccountService";
+
+async function getApiRequests() {
+  try {
+    const session = await auth();
+    const requiredRequests = await Promise.all([
+      getFileTypeGroupTestApi({namespace: "OrganizationOfficialDocuments"}, session),
+    ]);
+    const optionalRequests = await Promise.allSettled([]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    lang: string;
+  };
+}) {
+  const {lang} = params;
+  const {languageData} = await getResourceData(lang);
+  const apiRequests = await getApiRequests();
+
+  if ("message" in apiRequests) {
+    return <ErrorComponent languageData={languageData} message={apiRequests.message} />;
+  }
+
+  const [fileTypeGroupTestResponse] = apiRequests.requiredRequests;
+  return (
+    <FileUpload
+      backendUrl={process.env.BASE_URL || ""}
+      propertyId="dd1e83c0-57a8-8439-c731-3a17f2dbc603"
+      ruleset={fileTypeGroupTestResponse.data as unknown as Ruleset}
+    />
+  );
+}
