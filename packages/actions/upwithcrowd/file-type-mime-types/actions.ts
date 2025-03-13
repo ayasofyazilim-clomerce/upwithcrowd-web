05ae44c932fa6ed9dc09cafb43865102ce140af3
Eren <ahmeterenodaci@gmail.com>
2025-03-13 10:17:01 +0300

    feat: add API actions for managing file type MIME types

--- packages/actions/upwithcrowd/file-type-mime-types/actions.ts
+++ packages/actions/upwithcrowd/file-type-mime-types/actions.ts
@@ -1,14 +1,14 @@
 "use server";
 
-import type {GetApiFileRelationsData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import type {GetApiFileTypeMimeTypesData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
 import {structuredError, structuredSuccessResponse} from "@repo/utils/api";
 import type {Session} from "next-auth";
 import {getUpwithcrowdClient} from "../lib";
 
-export async function getFileRelationsApi(data: GetApiFileRelationsData, session: Session | null) {
+export async function getFileTypeMimeTypesApi(data: GetApiFileTypeMimeTypesData, session: Session | null) {
   try {
     const client = await getUpwithcrowdClient(session);
-    const response = await client.fileRelations.getApiFileRelations(data);
+    const response = await client.fileTypeMimeTypes.getApiFileTypeMimeTypes(data);
     return structuredSuccessResponse(response);
   } catch (error) {
     throw structuredError(error);
--- packages/actions/upwithcrowd/file-type-mime-types/delete-actions.ts
+++ packages/actions/upwithcrowd/file-type-mime-types/delete-actions.ts
@@ -1,13 +1,13 @@
 "use server";
 
-import type {DeleteApiFileRelationsByIdData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import type {DeleteApiFileTypeMimeTypesByIdData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
 import {structuredError, structuredResponse} from "@repo/utils/api";
 import {getUpwithcrowdClient} from "../lib";
 
-export async function deleteFileRelationsByIdApi(data: DeleteApiFileRelationsByIdData) {
+export async function deleteFileTypeMimeTypesByIdApi(data: DeleteApiFileTypeMimeTypesByIdData) {
   try {
     const client = await getUpwithcrowdClient();
-    const dataResponse = await client.fileRelations.deleteApiFileRelationsById(data);
+    const dataResponse = await client.fileTypeMimeTypes.deleteApiFileTypeMimeTypesById(data);
     return structuredResponse(dataResponse);
   } catch (error) {
     return structuredError(error);
