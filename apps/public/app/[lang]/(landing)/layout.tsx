fd2107bebcd7bb1801f43b9e91aad8d2190332f8
muhammed-gumus <mr.silver.mg@gmail.com>
2025-03-13 09:23:20 +0300

    feat: enhance layout component to include notification handling based on user session

--- apps/public/app/[lang]/(landing)/layout.tsx
+++ apps/public/app/[lang]/(landing)/layout.tsx
@@ -1,10 +1,21 @@
+import {auth} from "@repo/utils/auth/next-auth";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function Layout({children}: {children: React.ReactNode}) {
+export default async function Layout({children}: {children: React.ReactNode}) {
+  const session = await auth();
+  let notification;
+  if (session && process.env.NOVU_APP_IDENTIFIER && process.env.NOVU_APP_URL) {
+    notification = {
+      appId: process.env.NOVU_APP_IDENTIFIER,
+      appUrl: process.env.NOVU_APP_URL,
+      subscriberId: session.user?.sub || "",
+      langugageData: {},
+    };
+  }
   return (
     <section className="overflow-hidden">
-      <Header appName={process.env.APPLICATION_NAME || "UPWITHCROWD"} />
+      <Header appName={process.env.APPLICATION_NAME || "UPWITHCROWD"} notification={notification} />
       <main className="flex-grow">{children}</main>
       <Footer />
     </section>
