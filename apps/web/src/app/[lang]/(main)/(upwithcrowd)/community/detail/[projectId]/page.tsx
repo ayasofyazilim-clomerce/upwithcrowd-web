b4815ba693695b783f636241c0a4209496a14a8e
Eren <ahmeterenodaci@gmail.com>
2025-03-14 09:56:51 +0300

    feat: implement file management table and API integration for community detail page

---
+++ apps/web/src/app/[lang]/(main)/(upwithcrowd)/community/detail/[projectId]/page.tsx
@@ -0,0 +1,48 @@
+"use server";
+
+import type {GetApiFileData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import {getPublicFileApi} from "@repo/actions/upwithcrowd/public-file/action";
+import ErrorComponent from "@repo/ui/components/error-component";
+import {structuredError} from "@repo/utils/api";
+import {isRedirectError} from "next/dist/client/components/redirect";
+import {getResourceData} from "@/language-data/core/Default";
+import FileTable from "../_components/table";
+
+async function getApiRequests(searchParams: GetApiFileData) {
+  try {
+    const requiredRequests = await Promise.all([getPublicFileApi(searchParams)]);
+    const optionalRequests = await Promise.allSettled([]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
+
+export default async function Page({
+  params,
+  searchParams,
+}: {
+  params: {
+    lang: string;
+    projectId: string;
+  };
+  searchParams?: GetApiFileData;
+}) {
+  const {lang} = params;
+  const {languageData} = await getResourceData(lang);
+
+  const apiRequests = await getApiRequests({
+    ...searchParams,
+    relatedEntity: "Member",
+    relatedId: params.projectId,
+  });
+  if ("message" in apiRequests) {
+    return <ErrorComponent languageData={languageData} message={apiRequests.message} />;
+  }
+  const [fileResponse] = apiRequests.requiredRequests;
+
+  return <FileTable fileResponse={fileResponse.data} />;
+}
---
+++ apps/web/src/app/[lang]/(main)/(upwithcrowd)/community/detail/_components/file-table-data.tsx
@@ -0,0 +1,135 @@
+import {
+  $UpwithCrowd_Files_FileResponseListDto,
+  type UpwithCrowd_Files_FileResponseListDto,
+} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import {putFileValidationByIdApi} from "@repo/actions/upwithcrowd/file/put-actions";
+import type {TanstackTableCreationProps} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/types";
+import {tanstackTableCreateColumnsByRowData} from "@repo/ayasofyazilim-ui/molecules/tanstack-table/utils";
+import {handlePutResponse} from "@repo/utils/api";
+import {CircleCheck, DownloadIcon, FileTextIcon, Trash} from "lucide-react";
+import type {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
+import Link from "next/link";
+
+type FileTable = TanstackTableCreationProps<UpwithCrowd_Files_FileResponseListDto>;
+
+const fileColumns = (locale: string) => {
+  return tanstackTableCreateColumnsByRowData<UpwithCrowd_Files_FileResponseListDto>({
+    rows: $UpwithCrowd_Files_FileResponseListDto.properties,
+    languageData: {
+      fileTypeNamespace: "Dosya Tipi",
+    },
+    faceted: {
+      fileTypeNamespace: {
+        options: [
+          {
+            icon: FileTextIcon,
+            iconClassName: "text-blue-500 size-6",
+            value: "ProjectImages",
+            label: "Proje Resimleri",
+          },
+        ],
+      },
+    },
+    custom: {
+      fullPath: {
+        content(row) {
+          return (
+            <Link
+              className="flex items-center gap-2 text-blue-600"
+              href={row.fullPath || ""}
+              rel="noopener noreferrer"
+              target="_blank">
+              <DownloadIcon className="size-6" />
+            </Link>
+          );
+        },
+      },
+    },
+    badges: {
+      isValidated: {
+        hideColumnValue: true,
+        values: [
+          {
+            label: "Onaylandı",
+            badgeClassName: "text-primary-foreground bg-primary border-transparent",
+            conditions: [
+              {
+                when: (value) => Boolean(value),
+                conditionAccessorKey: "isValidated",
+              },
+            ],
+          },
+          {
+            label: "Onaylanmadı",
+
+            badgeClassName: "text-destructive-foreground bg-destructive border-transparent",
+            conditions: [
+              {
+                when: (value) => !value,
+                conditionAccessorKey: "isValidated",
+              },
+            ],
+          },
+        ],
+      },
+    },
+    config: {
+      locale,
+    },
+  });
+};
+
+function fileTable(router: AppRouterInstance) {
+  const table: FileTable = {
+    columnVisibility: {
+      type: "hide",
+      columns: ["fileId", "validatedUser", "validatedType"],
+    },
+    fillerColumn: "fileDescription",
+    columnOrder: [
+      "isValidated",
+      "fileTypeNamespace",
+      "mimeType",
+      "fileDescription",
+      "documentNumber",
+      "documentOriginator",
+      "fullPath",
+    ],
+
+    rowActions: [
+      {
+        cta: "Onayla",
+        onClick(row) {
+          void putFileValidationByIdApi({
+            id: row.fileId || "",
+            isValidated: true,
+          }).then((response) => {
+            handlePutResponse(response, router);
+          });
+        },
+        type: "simple",
+        icon: CircleCheck,
+        actionLocation: "row",
+        condition: (row) => !row.isValidated,
+      },
+      {
+        cta: "Delete",
+        onClick() {
+          // eslint-disable-next-line no-alert -- it's an example
+          alert("Delete");
+        },
+        type: "simple",
+        icon: Trash,
+        actionLocation: "row",
+      },
+    ],
+  };
+  return table;
+}
+
+export const tableData = {
+  projects: {
+    columns: fileColumns,
+    table: fileTable,
+  },
+};
