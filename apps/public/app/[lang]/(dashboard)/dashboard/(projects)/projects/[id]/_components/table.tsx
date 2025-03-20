124143b59b725a2a22f6e18d4c40bcde0c36a3c0
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-03-20 11:27:26 +0300

    feat(ProjectInvestorTable): enable header display for custom amount column

--- apps/public/app/[lang]/(dashboard)/dashboard/(projects)/projects/[id]/_components/table.tsx
+++ apps/public/app/[lang]/(dashboard)/dashboard/(projects)/projects/[id]/_components/table.tsx
@@ -22,6 +22,7 @@ export default function ProjectInvestorTable({investorResponse}: ProjectInvestor
     custom: {
       amount: {
         content: (row) => AmountRow(row),
+        showHeader: true,
       },
     },
     languageData: {
