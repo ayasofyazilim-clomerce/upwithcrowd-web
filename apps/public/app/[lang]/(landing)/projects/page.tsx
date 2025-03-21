3fdcb64f4a2186ad3a6f3ce724db239c4ab5214e
Ertuğrulcan Bakas <ecbakas@gmail.com>
2025-03-21 09:50:14 +0300

    feat(pagination): project list pagination

--- apps/public/app/[lang]/(landing)/projects/page.tsx
+++ apps/public/app/[lang]/(landing)/projects/page.tsx
@@ -1,38 +1,48 @@
-import type {UpwithCrowd_Projects_FundCollectionType} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
+import type {GetApiPublicProjectProjectListData} from "@ayasofyazilim/upwithcrowd-saas/UPWCService";
 import {getPublicProjectsApi} from "@repo/actions/upwithcrowd/public-project/action";
+import ErrorComponent from "@repo/ui/components/error-component";
+import {structuredError} from "@repo/utils/api";
+import {isRedirectError} from "next/dist/client/components/redirect";
+import {getResourceData} from "@/language/core/Default";
 import LandingHero from "@/components/landing-hero";
 import FilterSelector from "./_components/filter-selector";
 import ListedProjectCard from "./_components/listed-project-card";
 import SearchBar from "./_components/search-bar";
 import SortSelector from "./_components/sort-selector";
+import Pagination from "./_components/pagination";
+
+async function getApiRequests(params: GetApiPublicProjectProjectListData) {
+  try {
+    const requiredRequests = await Promise.all([getPublicProjectsApi({...params, maxResultCount: 10})]);
+    const optionalRequests = await Promise.allSettled([]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
 
 export default async function Page({
+  params,
   searchParams,
 }: {
-  searchParams: {
-    sorting?: string;
-    fundCollectionType?: UpwithCrowd_Projects_FundCollectionType;
-    dateFilter?: string;
-    projectName?: string;
-  };
+  params: {lang: string};
+  searchParams: GetApiPublicProjectProjectListData;
 }) {
+  const {lang} = params;
   const title = "Yenilikçi Projeleri Keşfet";
   const description =
     "Yaratıcılık ve inovasyon dünyasını keşfedin. Geleceği şekillendiren ve dünya çapında toplulukları etkileyen projelere destek olun.";
-
-  const reqBody = {};
-
-  Object.keys(searchParams).forEach((key) => {
-    if (searchParams[key as keyof typeof searchParams]) {
-      Object.assign(reqBody, {[key]: searchParams[key as keyof typeof searchParams]});
-    }
-  });
-
-  const projectsResponse = await getPublicProjectsApi(reqBody);
-
-  if (projectsResponse.type !== "success") return <>Sonuç bulunamadı.</>;
+  const {languageData} = await getResourceData(lang);
+  const apiRequests = await getApiRequests(searchParams);
+  if ("message" in apiRequests) {
+    return <ErrorComponent languageData={languageData} message={apiRequests.message} />;
+  }
+  const [projectsResponse] = apiRequests.requiredRequests;
   const projects = projectsResponse.data.items || [];
-
+  const totalCount = projectsResponse.data.totalCount || 0;
   return (
     <div className="bg-background min-h-screen">
       <section className="px-4 py-8 md:px-6 md:py-12 lg:py-20">
@@ -62,6 +72,7 @@ export default async function Page({
               />
             ))}
           </div>
+          <Pagination maxResultCount={10} totalCount={totalCount} />
         </div>
       </section>
     </div>
