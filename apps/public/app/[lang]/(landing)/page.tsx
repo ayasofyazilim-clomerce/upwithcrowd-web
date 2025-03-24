4f007a806309198c84385db1ca8fc645d6ea7fff
Eren <ahmeterenodaci@gmail.com>
2025-03-24 15:39:57 +0300

    fix: refactor API requests in landing page and enhance ProjectSlider component to handle empty states

--- apps/public/app/[lang]/(landing)/page.tsx
+++ apps/public/app/[lang]/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import {getPublicProjectsApi} from "@repo/actions/upwithcrowd/public-project/action";
-import {ProjectSlider} from "@/components/project-slider";
+import {structuredError} from "@repo/utils/api";
+import {isRedirectError} from "next/dist/client/components/redirect";
 import BlogPreview from "@/components/blog-preview";
 import CallToAction from "@/components/call-to-action";
 import DetailsSection from "@/components/details-section";
@@ -7,18 +8,31 @@ import ExpertTeam from "@/components/expert-team";
 import HeroSection from "@/components/hero-section";
 import MissionsSection from "@/components/mission-section";
 import OrganizationNumbers from "@/components/organization-numbers";
+import {ProjectSlider} from "@/components/project-slider";
 import ServicesSection from "@/components/services-section";
 import Testimonials from "@/components/testimoni";
 
+async function getApiRequests() {
+  try {
+    const requiredRequests = await Promise.all([getPublicProjectsApi()]);
+    const optionalRequests = await Promise.allSettled([]);
+    return {requiredRequests, optionalRequests};
+  } catch (error) {
+    if (!isRedirectError(error)) {
+      return structuredError(error);
+    }
+    throw error;
+  }
+}
+
 export default async function Page() {
-  const publicProjectsResponse = await getPublicProjectsApi({});
+  const apiRequests = await getApiRequests();
+
   return (
     <div className="bg-background min-h-screen">
       <main>
         <HeroSection />
-        <ProjectSlider
-          projects={publicProjectsResponse.type !== "success" ? [] : publicProjectsResponse.data.items || []}
-        />
+        <ProjectSlider projects={"message" in apiRequests ? [] : apiRequests.requiredRequests[0].data.items || []} />
         <DetailsSection />
         <ServicesSection />
         <MissionsSection />
